0269d2ca57d32acb94f466cefeafb7093ebbb396
VENKAT-JAN26 <venkatklsr@gmail.com>
2024-07-02 00:47:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { data } from "./data.js";
+
+function App() {
+  const [search, setSearch] = useState("");
+
+  return (
+    <>
+      <div className="container">
+        <h4 className="text-primary mt-5">Filter Table Data</h4>
+        <form className="col-md-5">
+          <input
+            type="text" className="form-control"
+            placeholder="search Text"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+         </form>
+          <table className="table table-bordered table-striped mt-3">
+            <thead>
+              <tr>
+                <th>S.No </th>
+                <th>First Name </th>
+                <th>Last Name </th>
+                <th>Email</th>
+                <th>Phone</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data
+                .filter((item) => {
+                  return search === ""? item:
+                  item.first_name.toLowerCase().includes(search.toLowerCase()) ||
+                  item.last_name.toLowerCase().includes(search.toLowerCase()) ||
+                  item.email.toLowerCase().includes(search.toLowerCase())||
+                  item.phone.toLowerCase().includes(search);
+                })
+                .map((item, index) => (
+                  <tr key={index}>
+                     <td>{index+1}</td>
+                    <td>{item.first_name}</td>
+                    <td>{item.last_name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.phone}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+      
+      </div>
+    </>
+  );
+}
+
+export default App;
